a29b7c123704e9a3ff9c32842172b7967f88170e
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2021-04-25 21:47:27 -0400

    still making css changes

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -13,6 +13,7 @@ export class AllProducts extends React.Component {
   }
 
   render() {
+    let color = ''
     return (
       <div className="pokemonlist">
         {this.props.pokemon.map((pokemon) => {
@@ -26,7 +27,52 @@ export class AllProducts extends React.Component {
               <div className='here'>
                 <p>
                   {pokemon.types.map((type) => {
-                    return <div key={type}>{type}</div>;
+                    if(type === 'grass'){
+                      color = 'rgb(119, 199, 81)'
+                    }
+                    if(type === 'water'){
+                      color = 'rgb(101, 145, 240)'
+                    }
+                    if(type === 'poison'){
+                      color = 'rgb(160, 63, 160)'
+                    }
+                    if(type === 'ground'){
+                      color = 'rgb(225,191,101)'
+                    }
+                    if(type === 'bug'){
+                      color = 'rgb(170, 182, 34)'
+                    }
+                    if(type === 'fire'){
+                      color = 'rgb(239, 126, 50)'
+                    }
+                    if(type === 'fighting'){
+                      color = 'rgb(179, 59, 45)'
+                    }
+                    if(type === 'normal'){
+                      color = 'rgb(166, 168, 119)'
+                    }
+                    if(type === 'flying'){
+                      color = 'rgb(168, 145, 235)'
+                    }
+                    if(type === 'psychic'){
+                      color = 'rgb(249 ,84 , 140)'
+                    }
+                    if(type === 'electric'){
+                      color = 'rgb(243, 208, 44)'
+                    }
+                    if(type === 'fairy'){
+                      color = 'rgb(236, 153, 173)'
+                    }
+                    if(type === 'rock'){
+                      color = 'rgb(182, 161, 56)'
+                    }
+                    if(type === 'steel'){
+                      color = 'rgb(184, 185, 206)'
+                    }
+                    if(type === 'ice'){
+                      color = 'rgb(151, 215, 215)'
+                    }
+                    return <div key={type} id="type" style ={{background: color}}>{type}</div>;
                   })}
                 </p>
               </div>
